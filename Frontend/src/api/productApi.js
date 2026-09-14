f4954369eaa26f5c0c49dd72255bb932c88728d9
Anurag Bodkhe <158081238+AnuragBodkhe@users.noreply.github.com>
2026-09-14 17:01:54 +0530

    Delete Frontend directory

--- Frontend/src/api/productApi.js
+++
@@ -1,9 +0,0 @@
-import api from './axiosConfig';
-
-export const getAllProducts = () => api.get('/products');
-export const getProductById = (id) => api.get(`/products/${id}`);
-export const getProductsByCategory = (category) => api.get(`/products/category/${category}`);
-export const createProduct = (data) => api.post('/products', data);
-export const createProductsBulk = (data) => api.post('/products/list', data);
-export const updateProduct = (id, data) => api.put(`/products/${id}`, data);
-export const deleteProduct = (id) => api.delete(`/products/${id}`);
